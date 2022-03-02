401ba04949e103be070b49d1dc4cbb923da34e0f
25garcial <91680716+25garcial@users.noreply.github.com>
2022-03-02 14:14:52 -0600

    Create smarts.js

---
+++ smarts.js
@@ -0,0 +1,21 @@
+let select=document.querySelector("div");
+
+let newDiv=document.createElement("div");
+
+let content=document.createElement("p");
+
+content.textContent=("HEllo, this is javascript at work.");
+
+select.appendChild(newDiv);
+
+newDiv.appendChild(content);
+let x=0;
+
+console.log("hi");
+function click(){
+  x++
+  alert(x)
+}
+let selector=document.querySelector(".magic-button")
+selector.onclick=click
+  
